dd32f4346ec0e5de7f4e17cc0dd93fa563cac591
kemboi22 <kemboielvis22@gmail.com>
2024-05-31 02:07:04 +0300

    Added Correct images build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/nginx:stable-alpine3.17" \
+    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/documenso/documenso:latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
