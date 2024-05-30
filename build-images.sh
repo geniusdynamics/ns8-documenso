9b8128efa488a8bcdd4d3f9078b6107e209d8419
kemboi22 <kemboielvis22@gmail.com>
2024-05-31 02:44:34 +0300

    Corrected reponames and ghcr.io build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -11,24 +11,24 @@ set -e
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
-repobase="${REPOBASE:-ghcr.io/nethserver}"
+repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
-reponame="kickstart"
+reponame="documenso"
 
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-documenso container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-documenso; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-documenso -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-documenso \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
