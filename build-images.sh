9f8c9b8ebc07e2a98296c3585c8bfc4bf7f7bde6
Martin Bhuong <martin@genius.ke>
2024-08-13 08:27:05 +0200

    Implement App versioning, and set version to 1.6.0 -- 1.6.1 to follow soon

--- build-images.sh
+++ build-images.sh
@@ -14,6 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="documenso"
+APP_VERSION="v1.6.0"
 
 # Create a new empty container image
 container=$(buildah from scratch)
@@ -45,7 +46,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/documenso/documenso:latest" \
+    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/documenso/documenso:${APP_VERSION}" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
